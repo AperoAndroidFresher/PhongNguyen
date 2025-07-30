d21796ce151929ce003bf3b450a850e8748aad4f
NguyenHoaiPhong <phongnh@talent.apero.vn>
2025-07-30 17:27:31 +0700

    add coil dependency

--- MusicApp/app/build.gradle.kts
+++ MusicApp/app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation(libs.coil.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
