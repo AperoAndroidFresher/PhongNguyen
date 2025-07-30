c00c2a6b38b35a7044ed3db402af472517e56d57
NguyenHoaiPhong <phongnh@talent.apero.vn>
2025-07-30 17:52:31 +0700

    update TopNav (No use)

--- MusicApp/app/src/main/java/com/hoaiphong/composeui/ui/navigation/TopLevelNavGraph.kt
+++ MusicApp/app/src/main/java/com/hoaiphong/composeui/ui/navigation/TopLevelNavGraph.kt
@@ -4,11 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.navigation3.runtime.entry
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
+import com.hoaiphong.composeui.ui.navigation.MyInformationScreen
 import com.hoaiphong.composeui.ui.screen.home.ContentRed
 import com.hoaiphong.composeui.ui.screen.home.HomeScreen
 import com.hoaiphong.composeui.ui.screen.layout.SharedBottom
-import com.hoaiphong.composeui.ui.screen.myinfo.MyInformation
-import com.hoaiphong.composeui.ui.screen.mysong.PlaylistSong
+import com.hoaiphong.composeui.ui.screen.myinfo.InformationScreen
+import com.hoaiphong.composeui.ui.screen.mysong.PlaylistSongScreen
 
 @Composable
 fun TopLevelNavGraph(
@@ -51,7 +52,7 @@ fun TopLevelNavGraph(
                         }
                     }
                 ) {
-                    PlaylistSong()
+                    PlaylistSongScreen()
                 }
             }
             entry<Song> {
@@ -68,7 +69,7 @@ fun TopLevelNavGraph(
                 }
             }
             entry<MyInformationScreen> {
-                MyInformation()
+                InformationScreen()
             }
         }
     )
