8fe3beec9bf7bd9d7570f9550af8a29cb5c7dda0
NguyenHoaiPhong <phongnh@talent.apero.vn>
2025-07-31 13:55:52 +0700

    Open permission for app to accept library

--- MusicApp/app/src/main/java/com/hoaiphong/composeui/MainActivity.kt
+++ MusicApp/app/src/main/java/com/hoaiphong/composeui/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.hoaiphong.composeui
 
-
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.rememberNavController
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.compose.AppTheme
 import com.hoaiphong.composeui.ui.navigation.AppNavGraph
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_AUDIO)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_MEDIA_AUDIO),
+                1
+            )
+        }
         setContent {
             AppTheme {
                 AppNavGraph()
@@ -19,4 +33,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
