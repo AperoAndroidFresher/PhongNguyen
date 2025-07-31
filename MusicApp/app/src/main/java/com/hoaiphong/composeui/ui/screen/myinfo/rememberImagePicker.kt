bd01c3a42a9ba80250270f31f6d323fba3eb29b1
NguyenHoaiPhong <phongnh@talent.apero.vn>
2025-07-31 08:26:16 +0700

    create rememberImagePicker

---
+++ MusicApp/app/src/main/java/com/hoaiphong/composeui/ui/screen/myinfo/rememberImagePicker.kt
@@ -0,0 +1,19 @@
+package com.hoaiphong.composeui.ui.screen.myinfo
+
+import android.content.Context
+import android.net.Uri
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+
+@Composable
+fun rememberImagePicker(
+    context: Context,
+    onImagePicked: (Uri?) -> Unit
+): ManagedActivityResultLauncher<String, Uri?> {
+    return rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = onImagePicked
+    )
+}
