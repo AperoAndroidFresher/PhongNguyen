e3ce4d7878abda15ec1a0fbd934de1f931940fc8
NguyenHoaiPhong <phongnh@talent.apero.vn>
2025-08-04 18:30:50 +0700

    import dependency for Room DB

--- MusicApp/app/build.gradle.kts
+++ MusicApp/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("kapt")
 }
 
 android {
@@ -40,6 +41,10 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    testImplementation(libs.androidx.room.testing)
     implementation(libs.coil.compose)
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
--- MusicApp/app/src/main/java/com/hoaiphong/composeui/ui/screen/library/LibraryViewModel.kt
+++ MusicApp/app/src/main/java/com/hoaiphong/composeui/ui/screen/library/LibraryViewModel.kt
@@ -2,49 +2,62 @@ package com.hoaiphong.composeui.ui.screen.library
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.application
+import androidx.lifecycle.viewModelScope
 import com.hoaiphong.composeui.comon.SongItemState
 import com.hoaiphong.composeui.data.model.PlaylistManager
 import com.hoaiphong.composeui.data.model.getAllMp3File
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 
 class LibraryViewModel(application: Application) : AndroidViewModel(application) {
-    private val context = application.applicationContext
     private val _uiState = MutableStateFlow(LibraryState())
     val uiState: StateFlow<LibraryState> = _uiState.asStateFlow()
 
     fun dispatch(intent: LibraryIntent) {
         when (intent) {
             is LibraryIntent.LoadLocalSongs -> {
-                val raw = getAllMp3File(context)
-                val wrapped = raw.map { SongItemState(it) }
-                _uiState.value = _uiState.value.copy(
-                    songs = wrapped,
-                    isLocalSelected = true
-                )
+                viewModelScope.launch(Dispatchers.IO) {
+                    val raw = getAllMp3File(application)
+                    val wrapped = raw.map { SongItemState(it) }
+                    _uiState.update {
+                        it.copy(
+                            songs = wrapped,
+                            isLocalSelected = true
+                        )
+                    }
+                }
             }
 
             is LibraryIntent.LoadRemoteSongs -> {
-                _uiState.value = _uiState.value.copy(
-                    songs = emptyList(),
-                    isLocalSelected = false
-                )
+                _uiState.update {
+                    it.copy(
+                        songs = emptyList(),
+                        isLocalSelected = false
+                    )
+                }
             }
 
             is LibraryIntent.ToggleDropdown -> {
-                val updated = _uiState.value.songs.mapIndexed { i, item ->
-                    if (i == intent.index) item.copy(isMenuExpanded = !item.isMenuExpanded)
-                    else item.copy(isMenuExpanded = false)
+                _uiState.update {
+                    val updated = it.songs.mapIndexed { i, item ->
+                        if (i == intent.index) item.copy(isMenuExpanded = !item.isMenuExpanded)
+                        else item.copy(isMenuExpanded = false)
+                    }
+                    it.copy(songs = updated)
                 }
-                _uiState.value = _uiState.value.copy(songs = updated)
             }
 
             is LibraryIntent.DismissDropdown -> {
-                val updated = _uiState.value.songs.map { it.copy(isMenuExpanded = false) }
-                _uiState.value = _uiState.value.copy(songs = updated)
+                _uiState.update {
+                    val updated = it.songs.map { it.copy(isMenuExpanded = false) }
+                    it.copy(songs = updated)
+                }
             }
 
             is LibraryIntent.ShowAddToPlaylistDialog -> {
@@ -65,6 +78,7 @@ class LibraryViewModel(application: Application) : AndroidViewModel(application)
                     )
                 }
             }
+
             is LibraryIntent.LoadPlaylists -> {
                 _uiState.update {
                     it.copy(playlists = PlaylistManager.getPlaylists())
@@ -72,4 +86,5 @@ class LibraryViewModel(application: Application) : AndroidViewModel(application)
             }
         }
     }
+
 }
