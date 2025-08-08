1ed9dfc9b99c9ed9dcf36e3d4da3aba27944cb09
HoaiPhong <phongnguyenhoai2003@gmail.com>
2025-08-08 21:09:05 +0700

    Add Convert Time From Long To String

--- MusicApp/app/src/main/java/com/hoaiphong/composeui/utils/ConvertDuration.kt
+++ MusicApp/app/src/main/java/com/hoaiphong/composeui/utils/ConvertDuration.kt
@@ -10,3 +10,10 @@ fun String.toDurationFormatted(): String {
         "0:00"
     }
 }
+
+fun formatTime(milliseconds: Long): String {
+    val totalSeconds = milliseconds / 1000
+    val minutes = totalSeconds / 60
+    val seconds = totalSeconds % 60
+    return "%02d:%02d".format(minutes, seconds)
+}
